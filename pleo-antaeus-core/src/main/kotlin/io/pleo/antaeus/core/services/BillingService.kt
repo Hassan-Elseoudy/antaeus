6bd5d5b874a0b7c9a5af5b405f4afaff5dcc1138
Hassan Elseoudy <hassan.elseoudy@gmail.com>
2021-11-06 13:11:13 +0200

    :clown_face: Mock Quartz Job and Test Functionality.

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,34 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.data.AntaeusDal
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import mu.KotlinLogging
+import org.quartz.Job
+import org.quartz.JobExecutionContext
 
-class BillingService(
-    private val paymentProvider: PaymentProvider
-) {
-// TODO - Add code e.g. here
+
+public class BillingService(private val paymentProvider: PaymentProvider,
+                            private val invoiceService: InvoiceService,
+                            private val dal: AntaeusDal) : Job {
+
+    val logger = KotlinLogging.logger { } //Maybe will be replaced with elastic search?
+
+
+    override fun execute(context: JobExecutionContext?) {
+        GlobalScope.launch {
+            charge()
+        }
+    }
+
+    private suspend fun charge() = coroutineScope {
+        print("This is a quartz job!");
+        val invoices = invoiceService.fetchPendingInvoices()
+        print(invoices)
+
+    }
+
+
 }

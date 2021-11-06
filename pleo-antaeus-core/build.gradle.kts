8c6b163efb01fabb0f9ccb3438462021c55e64be
Hassan Elseoudy <hassan.elseoudy@gmail.com>
2021-11-06 13:03:12 +0200

    :wrench: Update kotlin version and Quartz

--- pleo-antaeus-core/build.gradle.kts
+++ pleo-antaeus-core/build.gradle.kts
@@ -6,5 +6,7 @@ kotlinProject()
 
 dependencies {
     implementation(project(":pleo-antaeus-data"))
+    implementation("org.quartz-scheduler:quartz:2.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.5.2")
     api(project(":pleo-antaeus-models"))
 }
